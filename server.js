c5dcd7d586f2bdc6c44babb4df429e09a75e8f65
Laura Cole <lauracole1900@comcast.net>
2020-11-02 20:44:57 -0700

    server.js drafted

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const mysql = require("mysql");
 const express = require("express");
-const handleb = require("express-handlebars");
-const path = require("path");
+const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 
 const app = express();
 
 const PORT = process.env.PORT || 3005;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine("handlebars", handleb({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
+app.use(routes);
 
 
 app.listen(PORT, function() {
