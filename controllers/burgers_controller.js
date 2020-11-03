a91693c10d9df7002057d25a6c748f0fef6b8909
Laura Cole <lauracole1900@comcast.net>
2020-11-03 09:56:10 -0700

    working on button functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const burger = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create the `router` for the app, and export the `router` at the end of your file.
 
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,58 @@
+$(function() {
+  // Devour button listener
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+
+    var newDevourState = {
+      devoured: newDevour
+    };
+
+    // Update: PUT request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed devoured state to", newDevour);
+        location.reload();
+      }
+    );
+  });
+
+  // Form listener
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#bur").val().trim(),
+      devoured: $("[burger_name=devoured]:checked").val().trim()
+    };
+
+    // Create: POST request
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+
+  // Delete listener
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Delete: DELETE request
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        location.reload();
+      }
+    );
+  });
+});
